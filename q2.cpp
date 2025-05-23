c53ad1c9b9d0dddc895ca8092b31112c5cccd882
Anishdas007 <anish.das@bounteous.com>
2025-05-23 17:26:41 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -6,6 +6,7 @@
 using namespace std;
     int solve(vector<int>&nums,int t){
         unordered_map<int,int>mp;
+        mp[0]++;
         int sum=0;
         int count=0;
         for(int i=0;i<nums.size();i++){
