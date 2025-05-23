c4b8c5f2e7dde16247181149f7bfb7166fe4d44a
Anishdas007 <anish.das@bounteous.com>
2025-05-23 16:58:24 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+pair<int,int>solve(vector<int>&nums,int &t){
+    int i=0;
+    int j=nums.size()-1;
+    while(i<j){
+        if((nums[i]+nums[j])<t)i++;
+        else if((nums[i]+nums[j])>t)j--;
+        else return {i,j};
+    }
+    return {-1,-1};
+}
+int main() {
+    vector<int>nums={1,2,2,3,4,52,78};
+    int t=7;
+    pair<int,int>ans;
+    ans=solve(nums,t);
+    cout<<ans.first<<" "<<ans.second<<endl;
+    return 0;
+}
